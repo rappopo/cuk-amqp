3a717045be4e3be93da0321d89b2883f36ebf3fb
Ardhi Lukianto <ardhi@lukianto.com>
2019-01-11 11:27:54 +0700

    reg update

--- cuks/core/helper/get/channel.js
+++ cuks/core/helper/get/channel.js
@@ -4,7 +4,7 @@ module.exports = function (cuk) {
   const { _, helper } = cuk.pkg.core.lib
 
   return function (name) {
-    let names = helper('core:pkgTokenSplit')(name, 'Invalid amqp channel (%s)')
+    let names = helper('core:nameSplit')(name, false, 'Invalid amqp channel (%s)')
     let conn = _.get(cuk.pkg[names[0]], `cuks.amqp.channel.${names[1]}`)
     if (!conn) return
     return conn
--- cuks/core/helper/get/connector.js
+++ cuks/core/helper/get/connector.js
@@ -4,7 +4,7 @@ module.exports = function (cuk) {
   const { _, helper } = cuk.pkg.core.lib
 
   return function (name, follow = true) {
-    let names = helper('core:pkgTokenSplit')(name, 'Invalid amqp connector (%s)')
+    let names = helper('core:splitName')(name, false, 'Invalid amqp connector (%s)')
     let conn = _.get(cuk.pkg[names[0]], `cuks.amqp.connector.${names[1]}`)
     if (!conn) return
     if (follow && _.isString(conn)) conn = helper('amqp:getConnector')(conn, follow)
